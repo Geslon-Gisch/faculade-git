5bc3d860ccb1debd86da6df99774bf03dcdf60a0
Geslon Gisch <GeslonG@Outlook.com>
2023-03-30 21:41:56 -0300

    atividade feita

--- src/aulaGit/exercicio.java
+++ src/aulaGit/exercicio.java
@@ -1,9 +1,22 @@
 package aulaGit;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class exercicio {
 
 	public static void main(String[] args) {
-
-	}
-
+	        Locale.setDefault(new Locale("pt", "BR"));
+	        
+	        Calendar hoje = Calendar.getInstance();
+	        int mes = hoje.get(Calendar.MONTH);
+	        int hora = hoje.get(Calendar.HOUR_OF_DAY);
+	        int min = hoje.get(Calendar.MINUTE);
+	        mes++;
+	        DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL);
+	        Date data = new Date();
+	        System.out.println("A data é: " + df4.format(data) + ", o horario é: "+ hora + "hrs " + min + "min");
+	    }
 }
